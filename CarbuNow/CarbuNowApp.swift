6ee9a85a836b9742afda8dbece5a9e55769a609f
Yann CATTARIN <yann.cattarin@icloud.com>
2026-03-15 16:10:31 +0100

    Initial Commit

---
+++ CarbuNow/CarbuNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbuNowApp.swift
+//  CarbuNow
+//
+//  Created by Yann CATTARIN on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarbuNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
